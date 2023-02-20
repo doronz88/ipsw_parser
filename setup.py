1568fb16c7f23f7ac2a644a149f4b6989b99a2f8
doron zarhi <doron88@gmail.com>
2023-02-20 14:50:04 +0200

    setup: bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 BASE_DIR = Path(__file__).parent.resolve(strict=True)
-VERSION = '1.1.1'
+VERSION = '1.1.2'
 PACKAGE_NAME = 'ipsw_parser'
 PACKAGES = [p for p in find_packages() if not p.startswith('tests')]
 
